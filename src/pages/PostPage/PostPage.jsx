410def905be1012f4fd04005ac40366aa4168c74
Vakarian <fazmn7@gmail.com>
2023-10-25 13:36:56 +0800

    add post page components

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,8 +1,42 @@
+import PostAuthor from "../../components/PostAuthor/PostAuthor";
+import PostInteractions from "../../components/PostInteractions/PostInteractions";
+import Comments from "../../components/Comments/Comments";
+
 export default function PostPage () {
 
   return (
     <div>
-      Post Page
+      <div>
+        <ImageCarousel />
+        <PostInteractions />
+        <p>date</p>
+      </div>
+
+      <div>
+        <PostAuthor />
+        <p>post caption</p>
+        <Comments />
+        <PostComment />
+      </div>
+    </div>
+  );
+}
+
+function ImageCarousel () {
+
+  return (
+    <div>
+      images
+    </div>
+  );
+}
+
+function PostComment () {
+
+  return (
+    <div>
+      <textarea placeholder="Add a comment..." />
+      <button>Post</button>
     </div>
   );
 }
