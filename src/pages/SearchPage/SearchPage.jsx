063c154b21a56c090728843da27ba8ae090ddeb5
Vakarian <fazmn7@gmail.com>
2023-10-25 13:23:10 +0800

    add search page components

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 export default function SearchPage () {
+  const [results, setResults] = useState([]);
 
   return (
     <div>
-      Search Page
+      <SearchBar />  
+      {
+        results.length === 0 ? 
+        "No search results" :
+        results?.map(result => <SearchCard key={result} result={result}/>)
+      }
+    </div>
+  );
+}
+
+function SearchBar () {
+  return (
+    <>
+      <input placeholder="Search"/>
+      <hr />
+    </> 
+  );
+}
+
+function SearchCard ({ result }) {
+
+  return (
+    <div>
+      <img src='' alt='profile pic' />
+      <p>username</p>
+      <p>number of followers</p>
     </div>
   );
 }
